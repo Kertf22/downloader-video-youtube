d995bc3d2a71a5bb33a499e202d5f3676e3892b3
Paulo Hercílio <paulo.hercilio2015@gmail.com>
2022-10-07 22:53:08 -0300

    google adsense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9390787688776809"
-        crossOrigin="anonymous">
-          
-        </script>
+
 
       <Component {...pageProps} />
     </QueryClientProvider>)
